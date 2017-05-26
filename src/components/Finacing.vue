a514cccaea169b7711250faaa32282f71726b60d
leedou <1040241228@qq.com>
2017-05-26 14:44:09 +0800

    up

--- src/components/Finacing.vue
+++ src/components/Finacing.vue
@@ -1,6 +1,108 @@
 <template>
   <div class="finacing">
-    <p>理财</p>
+    <div class="item">
+      <h3>魅宝新人专享<span>热</span></h3>
+      <div class="item_content">
+        <div class="item_left">
+          <h2>12.12</h2>
+          <p>预计年化收益</p>
+        </div>
+        <div class="item_center">
+          <h2>7</h2>
+          <p>理财期限</p>
+        </div>
+        <div class="item_right">
+          <h2>立即抢购</h2>
+          <p>已抢购1%</p>
+        </div>
+      </div>
+    </div>  
+    <div class="item">
+      <h3>魅活宝<span>热</span></h3>
+      <div class="item_content">
+        <div class="item_left">
+          <h2>4.0</h2>
+          <p>七日年化收益</p>
+        </div>
+        <div class="item_center">
+          <h2 style="font-size:1.2rem;">活期</h2>
+          <p>理财期限</p>
+        </div>
+        <div class="item_right">
+          <h2>立即抢购</h2>
+          <p></p>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>看涨棉花组合稳收2%</h3>
+      <div class="item_content">
+        <div class="item_left">
+          <h2>2.0~22.0</h2>
+          <p>预计年化收益</p>
+        </div>
+        <div class="item_center">
+          <h2>1</h2>
+          <p>理财期限</p>
+        </div>
+        <div class="item_right">
+          <h2>立即抢购</h2>
+          <p>已抢购1%</p>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>看涨螺纹刚组合</h3>
+      <div class="item_content">
+        <div class="item_left">
+          <h2>2.5</h2>
+          <p>预计年化收益</p>
+        </div>
+        <div class="item_center">
+          <h2>15</h2>
+          <p>理财期限</p>
+        </div>
+        <div class="item_right">
+          <h2>立即抢购</h2>
+          <p>已抢购1%</p>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>魅宝新人专享<span>热</span></h3>
+      <div class="item_content">
+        <div class="item_left">
+          <h2>12.12</h2>
+          <p>预计年化收益</p>
+        </div>
+        <div class="item_center">
+          <h2>7</h2>
+          <p>理财期限</p>
+        </div>
+        <div class="item_right">
+          <h2>立即抢购</h2>
+          <p>已抢购1%</p>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <h3>魅宝新人专享<span>热</span></h3>
+      <div class="item_content">
+        <div class="item_left">
+          <h2>12.12</h2>
+          <p>预计年化收益</p>
+        </div>
+        <div class="item_center">
+          <h2>7</h2>
+          <p>理财期限</p>
+        </div>
+        <div class="item_right">
+          <h2>立即抢购</h2>
+          <p>已抢购1%</p>
+        </div>
+      </div>
+    </div>
+    
   </div>
 </template>
 
@@ -11,6 +113,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="sass" scoped>
+@import "../assets/style/finacing.scss"
 </style>
