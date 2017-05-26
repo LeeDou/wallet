4fcae75fe5f0e1d9a09eaee97c2af0242066c6bf
leedou <1040241228@qq.com>
2017-05-26 15:05:17 +0800

    up

--- src/components/Secure.vue
+++ src/components/Secure.vue
@@ -1,6 +1,59 @@
 <template>
   <div class="secure">
-    <p>保险</p>
+    <div class="item">
+      <img src="../assets/img/insure1.png" alt="">
+      <div class="item_content">
+        <h3>碎屏保</h3>
+        <p>魅族原装屏保障，碎屏免费维修</p>
+        <p class="belong">众安保险</p>
+        <h5 class="value">￥69 <span>/年</span></h5>
+      </div>
+    </div>
+    <div class="item">
+      <img src="../assets/img/insure2.png" alt="">
+      <div class="item_content">
+        <h3>魅族手机延保服务</h3>
+        <p>故障免费维修，安心用更久</p>
+        <p class="belong">魅族官方</p>
+        <h5 class="value">￥39 <span>/年</span></h5>
+      </div>
+    </div>
+    <div class="item">
+      <img src="../assets/img/insure3.png" alt="">
+      <div class="item_content">
+        <h3>步步保</h3>
+        <p>每天5000步，免费获取10万元重疾保障</p>
+        <p class="belong">众安保险</p>
+        <h5 class="value">免费</h5>
+      </div>
+    </div>
+    <div class="item">
+      <img src="../assets/img/insure4.png" alt="">
+      <div class="item_content">
+        <h3>出行意外保障</h3>
+        <p>乘坐飞机|火车|汽车|轮船安全保障</p>
+        <p class="belong">众安保险</p>
+        <h5 class="value">免费</h5>
+      </div>
+    </div>
+    <div class="item">
+      <img src="../assets/img/insure1.png" alt="">
+      <div class="item_content">
+        <h3>碎屏保</h3>
+        <p>魅族原装屏保障，碎屏免费维修</p>
+        <p class="belong">众安保险</p>
+        <h5 class="value">￥69 <span>/年</span></h5>
+      </div>
+    </div>
+    <div class="item">
+      <img src="../assets/img/insure2.png" alt="">
+      <div class="item_content">
+        <h3>碎屏保</h3>
+        <p>魅族原装屏保障，碎屏免费维修</p>
+        <p class="belong">众安保险</p>
+        <h5 class="value">￥69 <span>/年</span></h5>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +64,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="sass" scoped>
+@import "../assets/style/secure.scss"
 </style>
