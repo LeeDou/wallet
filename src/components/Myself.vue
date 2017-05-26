5bd72cdc1cd54c08d64a23890b8d27711311075f
leedou <1040241228@qq.com>
2017-05-26 17:14:47 +0800

    up

--- src/components/Myself.vue
+++ src/components/Myself.vue
@@ -1,6 +1,45 @@
 <template>
   <div class="myself">
-    <p>我的</p>
+  	<div class="head">
+	  <div class="person">
+	    <img src="../assets/img/personimg.png" alt="个人图片">
+	  </div>
+	  <div class="nickname">
+	    <h1 class="name">kantle</h1>
+	    <p class="tel">188****3800</p>	
+	  </div>
+	  <div class="arrow">></div>
+	  <div class="sun">
+		<div class="assets">
+			<p>总资产</p>
+			<p>*****</p>
+		</div>
+		<div class="income">
+			<p>昨日收益</p>
+			<p>￥0.0</p>
+		</div>
+		<div class="sum_inc">
+			<p>累计收益</p>
+			<p>￥0.0</p>
+		</div>
+	</div>
+	</div>
+	
+  
+	<div class="message_list">
+	  <div class="message_item it_one">
+	  	<p>我的理财<span>0 ></span></p>
+	  	
+	  </div>
+	  <div class="message_item it_two">
+	  	<p>我的保单<span>0 ></span></p>
+	  	
+	  </div>
+	  <div class="message_item it_three">
+	  	<p>更多<span>></span></p>
+	  	
+	  </div>
+	</div>
   </div>
 </template>
 
@@ -11,6 +50,6 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style lang="sass" scoped>
+@import "../assets/style/myself.scss"
 </style>
